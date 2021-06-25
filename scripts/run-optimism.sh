be5ce68c468ed5df56b7c9a5ac01aa71f02a4623
Moody Salem <moody.salem@gmail.com>
2021-06-25 11:03:27 -0500

    increase block gas limit so tests can pass

--- scripts/run-optimism.sh
+++ scripts/run-optimism.sh
@@ -12,6 +12,9 @@ git clone https://github.com/ethereum-optimism/optimism.git
 cd optimism/ops
 git checkout master
 
+# Use 1B gas for the block gas limit
+sed -i  's/TARGET_GAS_LIMIT=11000000/TARGET_GAS_LIMIT=1000000000/g' envs/geth.env
+
 # enabling the Docker BuildKit is recommended
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
